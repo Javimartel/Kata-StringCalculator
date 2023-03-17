c8fefb056a18bb3bba8bf00bea6ef33c766525e3
Javi <javiermespino@gmail.com>
2023-03-17 09:36:23 +0000

    Test(green): should_return_error_message_if_separators_are_next_to_each_other

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,4 +1,11 @@
 export function add(numbers: string) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (i !== 0) {
+            if (numbers[i].match(/[,\n]/) && numbers[i-1].match(/[,\n]/)) {
+                throw new Error(`Number expected but '${numbers[i]}' found at position ${i}.`)
+            }
+        }
+    }
     const array = numbers.split(/[,\n]/);
     const sum = array.reduce((accumulator, currentNumber) => accumulator + parseFloat(currentNumber), 0)
     return numbers === '' ? "0" : sum.toString();
