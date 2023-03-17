d5e677ff34e7f4b7dcec6809c0bf385f791a4c9a
Javi <javiermespino@gmail.com>
2023-03-17 09:18:33 +0000

    Test(green): should_return_the_sum_of_the_numbers_separated_by_commas_and_newlines

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,5 +1,5 @@
 export function add(numbers: string) {
-    const array = numbers.split(",");
+    const array = numbers.split(/[,\n]/);
     const sum = array.reduce((accumulator, currentNumber) => accumulator + parseFloat(currentNumber), 0)
     return numbers === '' ? "0" : sum.toString();
 }
