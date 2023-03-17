52868abe3e54900f0edc8551fb6db5db73e278e1
Javi <javiermespino@gmail.com>
2023-03-17 08:49:29 +0000

    Test(green): should_return_the_sum_of_the_numbers_separated_by_commas

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,3 +1,8 @@
 export function add(numbers: string) {
-    return numbers === '' ? "0" : numbers;
+    const array = numbers.split(",");
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += parseInt(array[i]);
+    }
+    return numbers === '' ? "0" : sum.toString();
 }
