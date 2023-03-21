f01142b5aad8ed00d926fbcec35f97672b05ad1c
Javi <javiermespino@gmail.com>
2023-03-21 09:47:06 +0000

    Test(red): should_return_0_when_string_is_empty

--- src/test/stringCalculator.test.ts
+++ src/test/stringCalculator.test.ts
@@ -1,5 +1,7 @@
 import { add } from '../core/stringCalculator';
 
 describe('stringCalculator Tests', () => {
-    
+    it('should return "0" when string is empty', () => {
+        expect(add('')).toBe("0");
+    });
 });
