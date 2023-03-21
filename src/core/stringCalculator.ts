9c0d74462fa9e5365f58fe914bc91ea3ea244615
Javi <javiermespino@gmail.com>
2023-03-21 10:43:40 +0000

    Test(refactor): should_return_error_message_if_separators_are_next_to_each_other

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,11 +1,5 @@
 export function add(numbers: string) {
-    numbers.split('').forEach((character, index) => {
-        const currentCharacter = character.match(/[,\n]/);
-        const nextCharacter = numbers[index + 1]?.match(/[,\n]/);
-        if (currentCharacter && nextCharacter) {
-            throw new Error(`Number expected but '${nextCharacter}' found at position ${index + 1}.`);
-        }
-    });
+    checkIfSeparatorsAreTogether(numbers);
     const numbersSplitted = numbers.split(/[,\n]/);
     const summedNumbers = sumNumbersFrom(numbersSplitted);
     return numbers !== '' ? summedNumbers.toString() : "0";
@@ -15,4 +9,14 @@ const sumNumbersFrom = (numbersSplitted: string[]) => {
     return numbersSplitted.reduce((accumulator, currentNumber) => {
         return accumulator + parseFloat(currentNumber);
     }, 0);
+}
+
+const checkIfSeparatorsAreTogether = (numbers: string) => {
+    numbers.split('').forEach((character, index) => {
+        const currentCharacter = character.match(/[,\n]/);
+        const nextCharacter = numbers[index + 1]?.match(/[,\n]/);
+        if (currentCharacter && nextCharacter) {
+            throw new Error(`Number expected but '${nextCharacter}' found at position ${index + 1}.`);
+        }
+    });
 }
