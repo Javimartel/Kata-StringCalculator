9f9e10785ead4e2a1fb621aee80116a9319f154e
Javi <javiermespino@gmail.com>
2023-03-21 09:49:40 +0000

    Test(red) should_return_1_when_string_is_1

--- src/test/stringCalculator.test.ts
+++ src/test/stringCalculator.test.ts
@@ -4,4 +4,8 @@ describe('stringCalculator Tests', () => {
     it('should return "0" when string is empty', () => {
         expect(add('')).toBe("0");
     });
+
+    it('should return "1" when string is "1"', () => {
+        expect(add('1')).toBe("1");
+    });
 });
