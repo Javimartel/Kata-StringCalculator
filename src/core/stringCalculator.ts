2b299cd1a38acc98df47934f26f9921c0a6aeda1
Javi <javiermespino@gmail.com>
2023-03-21 10:03:39 +0000

    Test(refactor): should_return_the_sum_of_multiple_numbers

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,7 +1,11 @@
 export function add(numbers: string) {
     const numbersArray = numbers.split(',');
-    const sumNumbers = numbersArray.reduce((accumulator, currentNumber) => {
+    const sumNumbers = sumNumbersFromArray(numbersArray);
+    return numbers !== '' ? sumNumbers.toString() : "0";
+}
+
+const sumNumbersFromArray = (numbersArray: string[]) => {
+    return numbersArray.reduce((accumulator, currentNumber) => {
         return accumulator + parseFloat(currentNumber);
     }, 0);
-    return numbers !== '' ? sumNumbers.toString() : "0";
 }
