1f9ab141a3513640881b1e886c3cbe0df3808e3b
Javi <javiermespino@gmail.com>
2023-03-21 12:33:38 +0000

    Removed unnecessary semicolon

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -18,12 +18,12 @@ const checkIfSeparatorsAreTogetherFrom = (numbers: string) => {
         const nextCharacter = numbers[index + 1]?.match(/[,\n]/);
         if (currentCharacter && nextCharacter) {
             throw new Error(`Number expected but '${nextCharacter}' found at position ${index + 1}.`);
-        };
+        }
     });
 }
 
 const checkIfLastCharacterIsSeparatorFrom = (numbers: string) => {
     if (numbers.at(-1)?.match(/[,\n]/)) {
         throw new Error("Number expected but EOF found.");
-    };
+    }
 }
