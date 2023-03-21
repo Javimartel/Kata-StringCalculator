2632acf653a6f24d26dccd40e437520b9ca563be
Javi <javiermespino@gmail.com>
2023-03-21 10:44:55 +0000

    Test(red): should_return_error_message_if_a_number_is_missing_in_last_position

--- src/test/stringCalculator.test.ts
+++ src/test/stringCalculator.test.ts
@@ -24,4 +24,8 @@ describe('stringCalculator Tests', () => {
     it('should return error message if separators are next to each other', () => {
         expect(() => add("175.2,\n35")).toThrow("Number expected but '\n' found at position 6.");
     });
+
+    it('should return error message if a number is missing in last position', () => {
+        expect(() => add("1,3,")).toThrow("Number expected but EOF found.");
+    });
 });
