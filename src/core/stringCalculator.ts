8a12187364d7312525ac7d0e358b4c0dc48d223b
Javi <javiermespino@gmail.com>
2023-03-22 12:46:15 +0000

    Test(green): should_return_error_message_if_there_are_more_than_one_separator_custom

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -35,6 +35,11 @@ const extractNumbersAndCustomSeparatorFrom = (numbers: string) => {
     const separatorIndex = numbers.indexOf('\n');
     const separator = numbers.substring(2, separatorIndex);
     const numbersToSplit = numbers.substring(separatorIndex + 1);
+    numbersToSplit.split('').forEach((character, index) => {
+        if (character !== separator && isNaN(parseFloat(character))) {
+            throw new Error(`'${separator}' expected but '${character}' found at position ${index}.`);
+        }
+    });
     const numbersSplitted = numbersToSplit.split(separator);
     return numbersSplitted;
 }
